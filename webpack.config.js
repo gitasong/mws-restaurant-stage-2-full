fd4d757ad3b1f3c80bb6ec527c4dbf0f0123cee0
Nicole Freed <nicole.freed@gmail.com>
2018-08-17 08:03:44 -0700

    fix: Fix chunk paths to properly inject JS with new file names

--- webpack.config.js
+++ webpack.config.js
@@ -45,14 +45,14 @@ module.exports = {
       title: 'Restaurant Reviews, Stage 2',
       template: './client/src/index.html',  // origin file
       filename: 'index.html',  // destination file in public
-      chunks: ['main'],
+      chunks: ['js/main'],
       inject: 'body'
     }),
     new HtmlWebpackPlugin({
       title: 'Restaurant Reviews, Stage 2',
       template: './client/src/restaurant.html',  // origin file
       filename: 'restaurant.html',  // destination file in public
-      chunks: ['restaurant_info'],
+      chunks: ['js/restaurant_info'],
       inject: 'body'
     }),
     new CleanWebpackPlugin(['./client/public/*/'])
